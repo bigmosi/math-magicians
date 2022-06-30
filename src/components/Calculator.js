d33f1c3577030aeb420a3285635b4dd6a6ee8552
bigmosi <kinyeramo@gmail.com>
2022-06-30 10:55:26 +0300

    refactor codes to work with react hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,122 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [obj, setObj] = useState({ next: null, total: null });
+  const handleClick = (e) => {
+    setObj((obj) => calculate(obj, e.target.innerText));
+  };
 
-  handleClick(e) {
-    this.setState((state) => calculate(state, e.target.innerText));
-  }
+  const items = [
+    {
+      id: 1,
+      symbol: 'AC',
+    },
+    {
+      id: 2,
+      symbol: '+/-',
+    },
+    {
+      id: 3,
+      symbol: '%',
+    },
+    {
+      id: 4,
+      symbol: '÷',
+    },
+    {
+      id: 5,
+      symbol: '7',
+    },
+    {
+      id: 6,
+      symbol: '8',
+    },
+    {
+      id: 7,
+      symbol: '9',
+    },
+    {
+      id: 8,
+      symbol: 'x',
+    },
+    {
+      id: 9,
+      symbol: '4',
+    },
+    {
+      id: 10,
+      symbol: '5',
+    },
+    {
+      id: 11,
+      symbol: '6',
+    },
+    {
+      id: 12,
+      symbol: '-',
+    },
+    {
+      id: 13,
+      symbol: '1',
+    },
+    {
+      id: 14,
+      symbol: '2',
+    },
+    {
+      id: 15,
+      symbol: '3',
+    },
+    {
+      id: 16,
+      symbol: '+',
+    },
+    {
+      id: 17,
+      symbol: '0',
+    },
+    {
+      id: 18,
+      symbol: '.',
+    },
+    {
+      id: 19,
+      symbol: '=',
+    },
+  ];
 
-  render() {
-    const items = [
-      {
-        id: 1,
-        symbol: 'AC',
-      },
-      {
-        id: 2,
-        symbol: '+/-',
-      },
-      {
-        id: 3,
-        symbol: '%',
-      },
-      {
-        id: 4,
-        symbol: '÷',
-      },
-      {
-        id: 5,
-        symbol: '7',
-      },
-      {
-        id: 6,
-        symbol: '8',
-      },
-      {
-        id: 7,
-        symbol: '9',
-      },
-      {
-        id: 8,
-        symbol: 'x',
-      },
-      {
-        id: 9,
-        symbol: '4',
-      },
-      {
-        id: 10,
-        symbol: '5',
-      },
-      {
-        id: 11,
-        symbol: '6',
-      },
-      {
-        id: 12,
-        symbol: '-',
-      },
-      {
-        id: 13,
-        symbol: '1',
-      },
-      {
-        id: 14,
-        symbol: '2',
-      },
-      {
-        id: 15,
-        symbol: '3',
-      },
-      {
-        id: 16,
-        symbol: '+',
-      },
-      {
-        id: 17,
-        symbol: '0',
-      },
-      {
-        id: 18,
-        symbol: '.',
-      },
-      {
-        id: 19,
-        symbol: '=',
-      },
-    ];
-
-    const { total, next } = this.state;
-    return (
-      <>
-        <section className="container">
-          <div className="result">{next || total || 0}</div>
-          <section className="grid-container">
-            {items.map((item, index) => (
-              <button
-                type="button"
-                key={item.id}
-                className={`grid-item item${index + 1}`}
-                onClick={this.handleClick}
-              >
-                {item.symbol}
-              </button>
-            ))}
-          </section>
+  const { total, next } = obj;
+  return (
+    <>
+      <section className="container">
+        <div className="result">{next || total || 0}</div>
+        <section className="grid-container">
+          {items.map((item, index) => (
+            <button
+              type="button"
+              key={item.id}
+              className={`grid-item item${index + 1}`}
+              onClick={handleClick}
+            >
+              {item.symbol}
+            </button>
+          ))}
         </section>
-      </>
-    );
-  }
-}
+      </section>
+    </>
+  );
+};
 
 export default Calculator;
